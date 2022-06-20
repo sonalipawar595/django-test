789f05909ecc0786ce2d042c26b2c090ff48b268
sonalipawar595 <sonalipawar595@gmail.com>
2022-06-19 19:53:09 -1200

    add project

---
+++ PersonPrj/personapp/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+# Create your models here.
+
+class Person(models.Model):
+    name      = models.CharField(max_length=100)
+    age       = models.IntegerField()
+    gender    = models.CharField(max_length=100)
+
+class Employee(models.Model):
+    name  = models.CharField(max_length=100)
+    age   = models.IntegerField()
+
+    def __str__(self):
+        return self.name
+
+
